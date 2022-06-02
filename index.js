ed5ebf65098c0f00811bb993b65367aa6f6f6516
michael muliro <michael.muliro@student.moringaschool.com>
2022-06-02 11:05:42 +0300

    commit tree 66d9d2200cf52ff1f63edfc8af86e30291f19386

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+const messages = [];
+function writeCards(array, eventName){
+    for(let i = 0; i < array.length; i++){
+        messages[i]= `Thank you, ${array[i]}, for the wonderful ${eventName} gift!`;
+    }
+    return messages
+}
+
+function countDown(int){
+    while(int >= 0){
+        console.log(int)
+        int--
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
